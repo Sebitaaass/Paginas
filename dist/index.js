24ea6b1d7699bbc8b87252cad2eaefb79b7c50a7
Sebas <sebastianzonati@gmail.com>
2025-04-26 13:46:22 -0400

    Mover views al dist

--- dist/index.js
+++ dist/index.js
@@ -19,4 +19,7 @@ app.get('/', (req, res) => {
 // Iniciar servidor
 app.listen(PORT, () => {
     console.log('Servidor corriendo en http://localhost:', PORT);
+    app.set('views', path.join(__dirname, 'views')); // NO src/views
+app.set('view engine', 'ejs');
+
 });
